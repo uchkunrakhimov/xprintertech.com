84c28ab2b505426d4e45e78988113d17ae59285a
Uchkun Rakhimov <uchkunaddons@gmail.com>
2022-08-14 09:02:41 +0500

    v1.0

---
+++ jscript/darktheme.js
@@ -0,0 +1,8 @@
+document.documentElement.setAttribute("data-theme", "light");
+var themeSwitcher = document.getElementById("theme-switcher");
+
+themeSwitcher.onclick = function () {
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+  var switchToTheme = currentTheme === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-theme", switchToTheme);
+};
